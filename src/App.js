3f920e217330084061b64c7e6334d0aca9fba691
ngwatso <ngwatson79@yahoo.com>
2021-02-19 22:11:02 -0500

    created Login component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import './styles.scss';
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Login} />
+				<PrivateRoute
+					exact
+					path="/protected"
+					component={BubblePage}
+				/>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,98 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+	// * make a post request to retrieve a token from the api
+	// * when you have handled the token, navigate to the BubblePage route
 
-  useEffect(()=>{
-    axios
-      .delete(`http://localhost:5000/api/colors/1`, {
-        headers:{
-          'authorization': "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
-        }
-      })
-      .then(res=>{
-        axios.get(`http://localhost:5000/api/colors`, {
-          headers:{
-            'authorization': ""
-          }
-        })
-        .then(res=> {
-          console.log(res);
-        });
-        console.log(res);
-      })
-  });
+	const initialState = {
+		credentials: {
+			username: '',
+			password: '',
+		},
+	};
 
-  return (
-    <>
-      <h1>
-        Welcome to the Bubble App!
-        <p>Build a login page here</p>
-      </h1>
-    </>
-  );
+	const [state, setState] = useState(initialState);
+
+	const history = useHistory();
+
+	const handleChange = (e) => {
+		setState({
+			credentials: {
+				...state.credentials,
+				[e.target.name]: e.target.value,
+			},
+		});
+	};
+
+	const login = (e) => {
+		e.preventDefault();
+		axios.post('http://localhost:5000/api/login', state.credentials)
+			.then((res) => {
+				console.log('Login.js: axios post =====> ', res);
+				localStorage.setItem('token', res.data.payload);
+				history.push('/protected');
+			})
+			.catch((err) =>
+				console.err('Username or Password not valid.', err.message)
+			);
+	};
+
+	useEffect(() => {
+		axios.delete(`http://localhost:5000/api/colors/1`, {
+			headers: {
+				authorization:
+					'ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98',
+			},
+		}).then((res) => {
+			axios.get(`http://localhost:5000/api/colors`, {
+				headers: {
+					authorization: '',
+				},
+			}).then((res) => {
+				console.log(res);
+			});
+			console.log(res);
+		});
+	});
+
+	return (
+		<>
+			<h1>
+				Welcome to the Bubble App!
+				<p>Log In</p>
+			</h1>
+			<form onSubmit={login}>
+				<label>
+					username:
+					<input
+						name="username"
+						type="text"
+						value={state.username}
+						onChange={handleChange}
+					/>
+				</label>
+				<label>
+					password:
+					<input
+						name="password"
+						type="password"
+						value={state.password}
+						onChange={handleChange}
+					/>
+				</label>
+				<button>Log In</button>
+			</form>
+		</>
+	);
 };
 
 export default Login;
 
 //Task List:
-//1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
+// * 1. Build a form containing a username and password field.
+// * 2. Add whatever state nessiary for form functioning.
+// * 3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
 //4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
 //5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
